764eb18bad26ef0200e176670af6eea07edd8310
Squibs <Zachary.t.walton@gmail.com>
2020-08-18 10:10:56 -0600

    chore: add in styles

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,16 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { SwitchField } from '@latitude/common-components'
 
 export default function App() {
-  return <SwitchField />
+  const [toggle, setToggle] = useState(true)
+  return (
+    <SwitchField
+      label="label"
+      info="info"
+      value={toggle}
+      onChange={(value: boolean) => {
+        setToggle(value)
+      }}
+    />
+  )
 }
--- src/switch/SwitchField.tsx
+++ src/switch/SwitchField.tsx
@@ -1,20 +1,31 @@
 import React from 'react'
-import { Switch, Text, View } from 'react-native'
+import {
+  Switch,
+  Text,
+  View,
+  ViewStyle,
+  TextStyle,
+  StyleSheet,
+} from 'react-native'
 
 interface Props {
   label?: string
   info?: string
   value?: boolean
-  onChange?(): void
+  onChange?(value?: boolean): void
 }
 
 const SwitchField: React.FC<Props> = (props) => {
   const { label, info, value, onChange } = props
 
   return (
-    <View>
-      <Text accessible={true}>{label}</Text>
-      <Text accessible={true}>{info}</Text>
+    <View style={styles.container}>
+      <Text accessible={true} style={styles.labelText}>
+        {label}
+      </Text>
+      <Text accessible={true} style={styles.infoText}>
+        {info}
+      </Text>
 
       <Switch
         accessibilityLabel={label}
@@ -26,5 +37,31 @@ const SwitchField: React.FC<Props> = (props) => {
     </View>
   )
 }
+
+interface Style {
+  container: ViewStyle
+  labelText: TextStyle
+  infoText: TextStyle
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    width: '100%',
+    marginBottom: 45,
+    alignItems: 'flex-start',
+  },
+  labelText: {
+    fontFamily: 'HelveticaNeueMedium',
+    fontSize: 24,
+    color: 'black',
+    marginBottom: 5,
+  },
+  infoText: {
+    color: 'grey',
+    fontFamily: 'HelveticaNeueMedium',
+    fontSize: 12,
+    marginBottom: 10,
+  },
+})
 
 export default SwitchField
