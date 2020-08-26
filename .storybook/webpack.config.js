c2d43a83a6ad0fe2ac4f856fbab28d42df2d7888
Squibs <Zachary.t.walton@gmail.com>
2020-08-26 12:32:11 -0600

    chore: allow icons

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = async ({ config, mode }) => {
   config.module.rules.push(
     {
       test: /\.(js|jsx|ts|tsx)$/,
       include: [
-        path.resolve(__dirname, '../node_modules/react-native-elements/'),
-        path.resolve(__dirname, '../node_modules/react-native-ratings/'),
-        path.resolve(__dirname, '../node_modules/react-native-vector-icons/'),
+        path.resolve(__dirname, '../', 'node_modules/react-native-elements'),
+        path.resolve(__dirname, '../', 'node_modules/react-native-ratings'),
+        path.resolve(__dirname, '../', 'node_modules/react-native-status-bar-height'),
+        path.resolve(__dirname, '../', 'node_modules/react-native-vector-icons'),
         path.resolve(__dirname, '../'),
       ],
       use: {
@@ -29,10 +31,26 @@ module.exports = async ({ config, mode }) => {
       }
     },
     {
-      test: /\.ttf$/,
-      loader: 'url-loader',
-      include: path.resolve(__dirname, '../node_modules/react-native-vector-icons/'),
-    }
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      include: path.resolve(__dirname, '../'),
+      use: [
+        {
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]'
+          }
+        }
+      ],
+    },
+  );
+
+  config.plugins.push(
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, '../', 'node_modules/react-native-vector-icons/Fonts'),
+        to: 'assets/fonts'
+      }]
+    }),
   );
 
   config.resolve.extensions = ['.web.ts', '.ts', '.json', '.web.tsx', '.tsx', '.web.js', '.js', '.web.jsx', '.jsx'];
